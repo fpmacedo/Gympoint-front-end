c1f59c8e62463dd61fd4ce92026fa52c5d50c6bc
fpmacedo <lipe_macedo@msn.com>
2020-01-06 22:08:55 -0300

    Pagina de planos iniciada, falta formatar os valores para exibicao

--- src/pages/Plans/index.js
+++ src/pages/Plans/index.js
@@ -1,6 +1,81 @@
-import React from 'react';
-// import { Container } from './styles';
+import React, { useEffect, useState } from 'react';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import api from '~/Services/api';
+import { HeaderMenu, Table, EditButton, DeleteButton } from './styles';
 
 export default function Plans() {
-  return <h1>Plans</h1>;
+  const [plans, setPlans] = useState([]);
+
+  async function loadPlans() {
+    const response = await api.get(`plans`);
+
+    setPlans(response.data);
+  }
+
+  useEffect(() => {
+    loadPlans();
+  });
+
+  async function handleDelete(id) {
+    await api.delete(`plans/${id}`);
+  }
+
+  function confirmDelete(id) {
+    confirmAlert({
+      title: 'Confirmação de exclusão',
+      message: 'Você quer mesmo excluir esse plano?',
+      buttons: [
+        {
+          label: 'Sim',
+          onClick: () => handleDelete(id),
+        },
+        {
+          label: 'Não',
+          onClick: () => {},
+        },
+      ],
+    });
+  }
+
+  return (
+    <>
+      <HeaderMenu>
+        <span>Gerenciando alunos</span>
+
+        <aside>
+          <button type="button" name="cadastrar">
+            CADASTRAR
+          </button>
+        </aside>
+      </HeaderMenu>
+      <Table>
+        <thead>
+          <tr>
+            <th scope="col">TÍTULO</th>
+            <th scope="col">DURAÇÃO</th>
+            <th scope="col">VALOR p/ MÊS</th>
+          </tr>
+        </thead>
+        <tbody>
+          {plans.map(plan => (
+            <tr key={plan.id}>
+              <td>{plan.title}</td>
+              <td>{plan.duration}</td>
+              <td>R$ {plan.price}</td>
+              <td>
+                <EditButton type="button">editar</EditButton>
+                <DeleteButton
+                  type="button"
+                  onClick={() => confirmDelete(plan.id)}
+                >
+                  apagar
+                </DeleteButton>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  );
 }
---
+++ src/pages/Plans/styles.js
@@ -0,0 +1,96 @@
+import styled from 'styled-components';
+
+export const HeaderMenu = styled.div`
+  max-width: 1200px;
+  margin: 40px auto 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  span {
+    font-size: 24px;
+    font-weight: bold;
+    color: #444444;
+  }
+  aside {
+    display: flex;
+  }
+
+  button {
+    background: #ee4d64;
+    border: none;
+    border-radius: 4px;
+    width: 142px;
+    height: 36px;
+    color: #ffffff;
+    font-weight: bold;
+  }
+
+  input {
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    padding: 0 35px;
+    width: 210px;
+    border-radius: 4px;
+    margin-left: 10px;
+    text-align: left;
+  }
+`;
+
+export const Table = styled.table`
+  border-collapse: collapse;
+  border-radius: 4px;
+  margin: 40px auto 0 auto;
+  font-size: 16px;
+  max-width: 1200px;
+  width: 100%;
+  text-align: center;
+  background: #fff;
+  thead {
+    tr {
+      font-size: 18px;
+      color: #444444;
+      th {
+        padding: 30px;
+      }
+    }
+  }
+  tbody {
+    overflow: scroll;
+    color: #666666;
+    font-size: 16px;
+  }
+  tbody td {
+    border-bottom: 1px solid #eee;
+  }
+  tr:last-child td {
+    border-bottom: none;
+  }
+  tbody tr {
+    height: 60px;
+  }
+  tbody tr {
+    button {
+      margin-right: 5px;
+      border: none;
+      background: none;
+      font-weight: bold;
+    }
+  }
+  tbody tr a {
+    color: #4d85ee;
+  }
+`;
+
+export const EditButton = styled.button`
+  margin-right: 10px;
+  border: none;
+  background: none;
+  color: #4d85ee;
+`;
+
+export const DeleteButton = styled.button`
+  border: none;
+  margin-left: 10px;
+  background: none;
+  color: #ee4d64;
+`;
