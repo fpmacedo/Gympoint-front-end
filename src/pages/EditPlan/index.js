0cdada9d48f3d73e7e5f5ebf9f120e7030730a35
fpmacedo <lipe_macedo@msn.com>
2020-02-11 22:15:57 -0300

    tentei

--- src/pages/EditPlan/index.js
+++ src/pages/EditPlan/index.js
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { MdCheck, MdClose } from 'react-icons/md';
 import api from '~/Services/api';
-import { formatPrice, formatComma } from '~/util/format';
+import { formatPrice } from '~/util/format';
 import { Container, Nav, FormInserts, NumbersDiv, StringDiv } from './styles';
 
 import history from '~/Services/history';
@@ -15,29 +15,22 @@ export default function EditPlan() {
   const [title, setTitle] = useState([]);
   const [price, setPrice] = useState(0);
   const [duration, setDuration] = useState(0);
+  const [helpPrice, sethelpPrice] = useState('');
   const [totalPrice, setTotalPrice] = useState('');
 
   useEffect(() => {
-    setTotalPrice(formatPrice(price * duration));
-  }, [price, duration]);
+    setTotalPrice(formatPrice(helpPrice * duration));
+    console.tron.log(helpPrice);
+  }, [helpPrice, duration]);
 
   useEffect(() => {
     async function loadPlan() {
       const response = await api.get(`plans?id=${id}`);
       console.tron.log(response);
-      setPrice(response.data.price);
+      setPrice(formatPrice(response.data.price));
       setDuration(response.data.duration);
       setTitle(response.data.title);
-
-      // const initialData = {
-      //   title: response.data.title,
-      //   duration: response.data.duration,
-      //   price: `R$ ${formatComma(response.data.price)}`,
-      //   total: formatPrice(response.data.price * response.data.duration),
-      // };
-      // console.tron.log(initialData);
-
-      // setPlan(initialData);
+      setTotalPrice(formatPrice(response.data.price * response.data.duration));
     }
     loadPlan();
   }, [id]);
@@ -52,6 +45,7 @@ export default function EditPlan() {
     try {
       data.price = data.price.replace('R$', '');
       data.price = data.price.replace(',', '.');
+      data.price = data.price.replace(/\s/g, '');
       console.tron.log(data);
       await api.put(`/plans/${id}`, data);
       history.push('/plans');
@@ -107,10 +101,13 @@ export default function EditPlan() {
               <strong>PREÇO MENSAL</strong>
               <Input
                 name="price"
-                value={`R$ ${formatComma(price)}`}
-                onChange={e =>
-                  setPrice(e.target.value.replace('R$', '').replace(',', '.'))
-                }
+                value={price}
+                onChange={e => {
+                  setPrice(e.target.value);
+                  sethelpPrice(
+                    Number(e.target.value.replace(',', '.').replace('R$', ''))
+                  );
+                }}
               />
             </div>
 
--- src/util/format.js
+++ src/util/format.js
@@ -2,5 +2,3 @@ export const { format: formatPrice } = new Intl.NumberFormat('pt-BR', {
   style: 'currency',
   currency: 'BRL',
 });
-
-export const { format: formatComma } = new Intl.NumberFormat('pt-BR');
